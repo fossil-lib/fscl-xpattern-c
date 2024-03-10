18fb0ab5f0985240d12d1b6f699870e715c35a39
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 18:14:29 -0500

    Update observer.h

--- code/include/fossil/xpattern/observer.h
+++ code/include/fossil/xpattern/observer.h
@@ -33,22 +33,73 @@ typedef struct {
 } csubject;
 
 // =================================================================
-// create and erase
+// Create and Erase
 // =================================================================
+
+/**
+ * Create a subject for observation.
+ *
+ * @param subject The subject to create.
+ */
 void fscl_observe_create(csubject* subject);
+
+/**
+ * Erase a subject and its observers.
+ *
+ * @param subject The subject to erase.
+ */
 void fscl_observe_erase(csubject* subject);
+
+/**
+ * Update all observers of the subject with the provided data.
+ *
+ * @param subject The subject whose observers need to be updated.
+ * @param data    The data to update the observers.
+ */
 void fscl_observe_update_all(csubject* subject, void* data);
 
 // =================================================================
-// addintal functions
+// Additional Functions
 // =================================================================
+
+/**
+ * Add an observer to the subject.
+ *
+ * @param subject  The subject to which the observer is added.
+ * @param observer The observer to add.
+ */
 void fscl_observe_add_observer(csubject* subject, cobserver* observer);
+
+/**
+ * Remove an observer from the subject.
+ *
+ * @param subject  The subject from which the observer is removed.
+ * @param observer The observer to remove.
+ */
 void fscl_observe_remove_observer(csubject* subject, cobserver* observer);
+
+/**
+ * Notify all observers of the subject with the provided data.
+ *
+ * @param subject The subject whose observers need to be notified.
+ * @param data    The data to notify the observers.
+ */
 void fscl_observe_notify(csubject* subject, void* data);
+
+/**
+ * Erase all observers from the subject.
+ *
+ * @param subject The subject from which all observers are erased.
+ */
 void fscl_observe_erase_all(csubject* subject);
+
+/**
+ * Check if the subject has any observers.
+ *
+ * @param subject The subject to check for observers.
+ * @return        1 if there are observers, 0 otherwise.
+ */
 int fscl_observe_has_observers(csubject* subject);
-
-
 
 #ifdef __cplusplus
 }
