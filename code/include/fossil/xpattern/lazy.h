383942e58a6576948a4db8b8c82ef02100d00033
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 18:13:32 -0500

    Update lazy.h

--- code/include/fossil/xpattern/lazy.h
+++ code/include/fossil/xpattern/lazy.h
@@ -54,36 +54,130 @@ typedef struct {
 } clazy;
 
 // =================================================================
-// create and erase
+// Create and Erase
 // =================================================================
+
+/**
+ * Create a lazy object with the specified type.
+ *
+ * @param type The type of the lazy object.
+ * @return     The created lazy object.
+ */
 clazy fscl_lazy_create(clazy_type type);
-void fscl_lazy_erase(clazy *lazy);
+
+/**
+ * Erase a lazy object.
+ *
+ * @param lazy The lazy object to erase.
+ */
+void fscl_lazy_erase(clazy* lazy);
 
 // =================================================================
-// Jedi Dreamer force functions
+// Jedi Dreamer Force Functions
 // =================================================================
-void fscl_lazy_force(clazy *lazy);
-int fscl_lazy_force_int(clazy *lazy);
-bool fscl_lazy_force_bool(clazy *lazy);
-char fscl_lazy_force_char(clazy *lazy);
-const char* fscl_lazy_force_string(clazy *lazy);
+
+/**
+ * Force the evaluation of the lazy object.
+ *
+ * @param lazy The lazy object to force.
+ */
+void fscl_lazy_force(clazy* lazy);
+
+/**
+ * Force and return the integer value of the lazy object.
+ *
+ * @param lazy The lazy object to force.
+ * @return     The forced integer value.
+ */
+int fscl_lazy_force_int(clazy* lazy);
+
+/**
+ * Force and return the boolean value of the lazy object.
+ *
+ * @param lazy The lazy object to force.
+ * @return     The forced boolean value.
+ */
+bool fscl_lazy_force_bool(clazy* lazy);
+
+/**
+ * Force and return the character value of the lazy object.
+ *
+ * @param lazy The lazy object to force.
+ * @return     The forced character value.
+ */
+char fscl_lazy_force_char(clazy* lazy);
+
+/**
+ * Force and return the string value of the lazy object.
+ *
+ * @param lazy The lazy object to force.
+ * @return     The forced string value.
+ */
+const char* fscl_lazy_force_string(clazy* lazy);
 
 // =================================================================
-// addintal functions
+// Additional Functions
 // =================================================================
+
+/**
+ * Create a lazy sequence object.
+ *
+ * @return The created lazy sequence object.
+ */
 clazy fscl_lazy_sequence(void);
-int fscl_lazy_sequence_force(clazy *lazy, int n);
-void fscl_lazy_set_int(clazy *lazy, int value);
-void fscl_lazy_set_cstring(clazy *lazy, const char *value);
-void fscl_lazy_set_bool(clazy *lazy, bool value);
-void fscl_lazy_set_letter(clazy *lazy, char value);
-void fscl_lazy_conditional_eval(clazy *lazy, int condition);
-void fscl_lazy_map_int(clazy *lazy, int (*mapFunction)(int));
-void fscl_lazy_map_bool(clazy *lazy, bool (*mapFunction)(bool));
-void fscl_lazy_map_char(clazy *lazy, char (*mapFunction)(char));
-void fscl_lazy_map_cstring(clazy *lazy, const char* (*mapFunction)(const char*));
-void fscl_lazy_concat_cstrings(clazy *result, clazy *str1, clazy *str2);
-void fscl_lazy_print(clazy *lazy);
+
+/**
+ * Force and return the next value of the lazy sequence.
+ *
+ * @param lazy The lazy sequence object to force.
+ * @param n    Number of values to skip.
+ * @return     The forced integer value.
+ */
+int fscl_lazy_sequence_force(clazy* lazy, int n);
+
+/**
+ * Set the integer value of the lazy object.
+ *
+ * @param lazy  The lazy object to set.
+ * @param value The integer value to set.
+ */
+void fscl_lazy_set_int(clazy* lazy, int value);
+
+// Other lazy set functions ...
+
+/**
+ * Conditional evaluation of the lazy object based on the given condition.
+ *
+ * @param lazy      The lazy object to conditionally evaluate.
+ * @param condition The condition for evaluation.
+ */
+void fscl_lazy_conditional_eval(clazy* lazy, int condition);
+
+/**
+ * Map the lazy integer object using the provided mapping function.
+ *
+ * @param lazy          The lazy integer object to map.
+ * @param mapFunction   The mapping function for integers.
+ */
+void fscl_lazy_map_int(clazy* lazy, int (*mapFunction)(int));
+
+// Other lazy map functions ...
+
+/**
+ * Concatenate two lazy string objects and store the result in another lazy object.
+ *
+ * @param result The lazy object to store the concatenated result.
+ * @param str1   The first lazy string object.
+ * @param str2   The second lazy string object.
+ */
+void fscl_lazy_concat_cstrings(clazy* result, clazy* str1, clazy* str2);
+
+/**
+ * Print the forced value of the lazy object.
+ *
+ * @param lazy The lazy object to print.
+ */
+void fscl_lazy_print(clazy* lazy);
 
 #ifdef __cplusplus
 }
